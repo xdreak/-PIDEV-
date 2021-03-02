4d7e39a8c447fb1f7a3f04cfaccfda38a8fbad4b
jihengabsi <gabsijihen31@gmail.com>
2021-03-02 03:07:28 +0100

    crud1

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -16,11 +16,17 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 class FormationController extends AbstractController
 {
 
+  public function index(): Response
+    {
+        return $this->render('formation/index.html.twig', [
+            'controller_name' => 'FormationController',
+        ]);
+    }
      /**
      * @Route("/formation",name="formation")
      * @Method({"GET"})
      */
-    public function index(): Response
+    public function index1(): Response
     {
      $formations=$this->getDoctrine()->getRepository(Formation::class)->findAll();
      return $this->render('formation/index.html.twig',array ('formations'=>$formations));
@@ -80,8 +86,83 @@ class FormationController extends AbstractController
         return $this->render('formation/addFormation.html.twig',array('form'=>$form->createView()));
 
     }
+    /**
+     * @Route("/formation/modifier/{id}", name="edit_class")
+     * Method({"GET", "POST"})
+     */
+    public function edit(Request $request, $id) {
+      $Formation = new Formation();
+      $Formation = $this->getDoctrine()->getRepository(Formation::class)->find($id);
 
+      $form = $this->createFormBuilder($Formation)
+      ->add('Titre', TextareaType::class, array(
+        'label' => 'Titre',
+          'required' => true,
+          'attr' => array('class' => 'form-control')
+        ))
+             ->add('Date', DateType::class, array(
+                'label' => 'Date',
+                  'required' => true,
+                  'attr' => array('class' => 'form-control')
+         ))
+         ->add('Lieu', TextareaType::class, array(
+            'label' => 'Lieu',
+              'required' => true,
+              'attr' => array('class' => 'form-control')
+        ))
+        ->add('Description', TextareaType::class, array(
+            'label' => 'Description',
+              'required' => true,
+              'attr' => array('class' => 'form-control')
+        ))
+        ->add('Prix', TextareaType::class, array(
+            'label' => 'Prix',
+              'required' => true,
+              'attr' => array('class' => 'form-control')
+        ))
+        ->add('save', SubmitType::class, array(
+          'label' => 'Modifier',
+          'attr' => array('class' => 'btn btn-primary mt-3')
+        ))
+        ->getForm();
 
+      $form->handleRequest($request);
+
+      if($form->isSubmitted() && $form->isValid()) {
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->flush();
+
+        return $this->redirectToRoute('formation');
+      }
+
+      return $this->render('formation/modFormation.html.twig', array(
+        'form' => $form->createView()
+      ));
+    }
+ /**
+     * @Route("/formation/delete/{id}")
+     * @Method({"DELETE"})
+     */
+    public function delete(Request $request, $id) {
+      $Formation = $this->getDoctrine()->getRepository(Formation::class)->findOneById($id);
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->remove($Formation);
+      $entityManager->flush();
+
+      $response = new Response();
+      $response->send();
+      return $this->redirectToRoute('formation');
+    }
+
+/**
+     * @Route("/formation/{id}", name="formation_show")
+     */
+    public function show($id) {
+      $Formation = $this->getDoctrine()->getRepository(Formation::class)->find($id);
+
+      return $this->render('formation/affichFormation.html.twig', array('formation' => $Formation));
+    }
 
 
 
